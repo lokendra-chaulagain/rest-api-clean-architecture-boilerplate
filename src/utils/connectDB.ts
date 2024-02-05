75472e3ec533f8a752380415f2da6e27ef4f6762
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-02-05 16:18:24 +0545

    add DB connection retry method

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,16 +1,29 @@
 import mongoose from "mongoose";
 import config from "config";
 import logger from "./logger";
+import retry from "retry";
+
+const dbUri = config.get<string>("dbUri");
+
+// Database connection retry config
+const operation = retry.operation({
+  retries: 10, // Number of retries
+  factor: 1.5, // Exponential backoff factor
+  minTimeout: 500, // Minimum time between retries (in milliseconds)
+  maxTimeout: 5000, // Maximum time between retries (in milliseconds)
+  randomize: true, // Randomize the timeouts
+});
 
 export default async function connectDB() {
-  const dbUri = config.get<string>("dbUri");
-
-  try {
-    await mongoose.connect(dbUri);
-    logger.info("DB connected...");
-  } catch (error) {
-    logger.error("Could not connect to db");
-    setTimeout(connectDB, 5000);
-    // process.exit(1);
-  }
+  operation.attempt(async () => {
+    try {
+      await mongoose.connect(dbUri);
+      logger.info("DB connected...");
+    } catch (error: any) {
+      logger.error("Could not connect to db", error);
+      if (operation.retry(error)) {
+        return;
+      }
+    }
+  });
 }
